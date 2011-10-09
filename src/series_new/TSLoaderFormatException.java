424faa3586d59498b314ae7a6be5fb5d44aac989
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2011-10-09 13:10:35 +0200

    Copied localized exception resolution: series -> series_new

---
+++ src/series_new/TSLoaderFormatException.java
@@ -0,0 +1,40 @@
+//Copyright (C) 2011 Tomáš Vejpustek
+//Full copyright notice found in src/LICENSE.  
+package series_new;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+/**
+ * Signals wrong format of input when loading time series via an implementation of {@link TimeSeriesLoader}.
+ * In addition to error description (as of {@link TSLoaderException}) prints the line where error was encountered and its number.
+ * 
+ * @author Tomáš Vejpustek
+ *
+ */
+@SuppressWarnings("serial")
+public class TSLoaderFormatException extends TSLoaderException {
+	private int lineNum;
+	private String lineString;
+	
+	/**
+	 * Specifies error type, message and contents of line on which the error occurred (with its number). 
+	 * 
+	 * @param type Error type.
+	 * @param message Short descriptive message.
+	 * @param lineNum Number of line containing error.
+	 * @param lineString Contents of line containing error.
+	 */
+	public TSLoaderFormatException(String type, String message, int lineNum, String lineString) {
+		super(type, message);
+		this.lineNum = lineNum;
+		this.lineString = lineString;
+	}
+
+	@Override
+	public String getLocalizedMessage() {
+		ResourceBundle errors = ResourceBundle.getBundle("series_new.messages");
+		return errors.getString(getType()) + "\n" + MessageFormat.format(errors.getString("tsl_line"), lineNum, lineString);
+	}
+
+}
