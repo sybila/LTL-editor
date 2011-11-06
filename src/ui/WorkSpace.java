398464b6919dc85523776654733169f422900a20
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2011-11-06 10:56:08 +0100

    Repaired minor issue when dragging outside selected primitive would not cause to visually unselect it (refreshing display).

--- src/ui/WorkSpace.java
+++ src/ui/WorkSpace.java
@@ -81,14 +81,13 @@ public class WorkSpace extends JPanel implements ComponentListener, MouseMotionL
 					Point2D p = new Point2D.Double(e.getX(), e.getY());
 					if (active.contains(p)) {
 						active.startDrag(e);
-						refresh();
 					} else if (active.objectContains(p)) {
 						active.startMove(p);
-						refresh();
 					} else {
 						getModel().unselect();
 						active = null;
 					}
+					refresh();
 					getParentForm().setPrimitiveSelected(false);
 				}
 			}
